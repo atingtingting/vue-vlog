250dfde021b1eaf90f3921838b3352f584bfe6c4
atingtingting <365023166@qq.com>
2019-10-05 22:00:45 +0800

    from 0

---
+++ myBlog/src/components/common/assay.vue
@@ -0,0 +1,74 @@
+<template>
+    <div id="assay">
+      <div class="essayBox">
+        <h2>标题标题标题</h2>
+        <p class="time">最新发布时间：xxxxxxxxxx</p>
+        <el-image></el-image>
+        <el-image></el-image>
+        <el-image></el-image>
+        <p>内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容</p>
+        <p>内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容</p>
+        <p>内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容</p>
+        <div class="userBox">
+            <el-avatar size="small">匿</el-avatar>
+            <div>匿名</div>
+        </div>
+        <div class="col-com">
+            <el-button type="text" icon="el-icon-collection-tag"></el-button>
+            <el-button type="text" icon="el-icon-chat-dot-square"></el-button>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.essayBox {
+  position: relative;
+  width: 95%;
+  height: auto;
+  margin: 10px auto;
+  padding: 1px 0 30px 20px;
+  box-sizing: border-box;
+  background: #fcfcfc;
+  color: #335577;
+  .time {
+    color: #858585;
+    font-size: 10px;
+  }
+  p {
+    margin: 5px 0 5px 0;
+  }
+}
+.el-image {
+  width: 200px;
+  height: 160px;
+}
+.userBox {
+  position: relative;
+  margin: 30px 0 0 0;
+  div {
+    position: absolute;
+    top: 4px;
+    left: 35px;
+    font-size: 15px;
+  }
+}
+.col-com {
+  position: absolute;
+  bottom: 10px;
+  right: 20px;
+  .el-button {
+    font-size: 25px;
+    margin-left: 20px;
+  }
+}
+</style>
--- myBlog/src/components/home.vue
+++ myBlog/src/components/home.vue
@@ -5,24 +5,8 @@
       <el-button type="text" icon="el-icon-star-on">最多收藏</el-button>
       <el-button type="text" icon="el-icon-s-comment">最多评论</el-button>
     </div>
-    <div class="essayBox">
-      <h2>标题标题标题</h2>
-      <p class="time">最新发布时间：xxxxxxxxxx</p>
-      <el-image></el-image>
-      <el-image></el-image>
-      <el-image></el-image>
-      <p>内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容</p>
-      <p>内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容</p>
-      <p>内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容</p>
-      <div class="userBox">
-        <el-avatar size="small">匿</el-avatar>
-        <div>匿名</div>
-      </div>
-      <div class="col-com">
-        <el-button type="text" icon="el-icon-collection-tag"></el-button>
-        <el-button type="text" icon="el-icon-chat-dot-square"></el-button>
-      </div>
-    </div>
+    <assay></assay>
+    <assay></assay>
     <div class="pageBox">
     <el-pagination
       @size-change="handleSizeChange"
@@ -37,9 +21,11 @@
 </template>
 
 <script >
+import Assay from './common/assay'
 export default {
   name: 'home',
   components: {
+    'assay': Assay
   },
   data () {
     return {
@@ -73,48 +59,8 @@ export default {
     margin-right: 30px;
   }
 }
-.essayBox {
-  position: relative;
-  width: 95%;
-  height: auto;
-  margin: 0 auto;
-  padding: 1px 0 30px 20px;
-  box-sizing: border-box;
-  background: #fcfcfc;
-  color: #335577;
-  .time {
-    color: #858585;
-    font-size: 10px;
-  }
-  p {
-    margin: 5px 0 5px 0;
-  }
-}
-.el-image {
-  width: 200px;
-  height: 160px;
-}
-.userBox {
-  position: relative;
-  margin: 30px 0 0 0;
-  div {
-    position: absolute;
-    top: 4px;
-    left: 35px;
-    font-size: 15px;
-  }
-}
-.col-com {
-  position: absolute;
-  bottom: 10px;
-  right: 20px;
-  .el-button {
-    font-size: 25px;
-    margin-left: 20px;
-  }
-}
 .pageBox {
-  margin: 60px 0 0 0;
+  margin: 60px 0 60px 0;
   text-align: center;
 }
 </style>
